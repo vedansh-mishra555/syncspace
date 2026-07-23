a21d8ccd77db7d35985277210d56f92e84883a19
vedansh-mishra555 <vedanshmishra333@gmail.com>
2026-07-23 21:13:26 +0530

    Day 7: Added note creation form with React and Axios

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,90 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import API from "../services/api";
 
 function Home() {
   const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-  useEffect(() => {
-    async function fetchNotes() {
-      try {
-        console.log("Fetching notes...");
-
-        const response = await API.get("/notes");
-
-        console.log("Received:", response.data);
-
-        setNotes(response.data);
-      } catch (error) {
-        console.error("API Error:", error);
-      }
+  // Fetch all notes
+  const fetchNotes = useCallback(async () => {
+    try {
+      const res = await API.get("/notes");
+      setNotes(res.data);
+    } catch (error) {
+      console.log(error);
     }
-
-    fetchNotes();
   }, []);
 
+  // Load notes
+  useEffect(() => {
+    fetchNotes();
+  }, [fetchNotes]);
+
+  // Add Note
+  const addNote = async () => {
+    if (!title || !content) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    try {
+      await API.post("/notes", {
+        title,
+        content,
+      });
+
+      // Clear input fields
+      setTitle("");
+      setContent("");
+
+      // Refresh notes
+      fetchNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>📝 SyncSpace</h1>
+
+      <h2>Add Note</h2>
+
+      <input
+        type="text"
+        placeholder="Enter Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <textarea
+        placeholder="Enter Content"
+        rows="5"
+        cols="40"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={addNote}>Add Note</button>
+
+      <hr />
 
       <h2>My Notes</h2>
 
       <p>Total Notes: {notes.length}</p>
 
-    
-
       {notes.map((note) => (
         <div key={note._id}>
           <h3>{note.title}</h3>
           <p>{note.content}</p>
+          <hr />
         </div>
       ))}
     </div>
